602e804ebea1434deb4293e3b88cf2a1b7c750c1
taoting <335354289@qq.com>
2020-12-18 15:39:22 +0800

    modify gitignore

---
+++ app/config_demo.py
@@ -0,0 +1,8 @@
+# 定义数据库信息
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root" \
+                          "@127.0.0.1:3306/learning-platform" \
+                          "?charset=utf8mb4"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# 定义flask信息
+SECRET_KEY = '123456'
