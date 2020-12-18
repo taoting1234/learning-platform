194517f1ca5390ab729cffb50a419d9b7b5cdbd8
taoting <335354289@qq.com>
2020-12-18 15:38:34 +0800

    modify gitignore

--- app/config.py
+++
@@ -1,8 +0,0 @@
-# 定义数据库信息
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://learning_platform:LEARNING123platform" \
-                          "@newitd-w.mysql.rds.aliyuncs.com:3306/learning-platform" \
-                          "?charset=utf8mb4"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# 定义flask信息
-SECRET_KEY = 'aabbcc'
