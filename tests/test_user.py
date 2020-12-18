f4d3bd7e7ae22fa404064fe6f9657c630e704559
taoting <335354289@qq.com>
2020-12-18 09:36:09 +0800

    修改测试的结构

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,24 +1,4 @@
-import pytest
-from flask import Flask
-
-from app import config
-from app.models.user import User
-from flask_app import register_plugin, register_resource
-
-
-@pytest.fixture
-def client():
-    app = Flask(__name__)
-    app.config.from_object(config)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-    app.config['TESTING'] = True
-    register_resource(app)
-    register_plugin(app)
-
-    with app.test_client() as client:
-        with app.app_context():
-            User.create(username='admin', password='admin')
-        yield client
+from .base import client
 
 
 def test_user(client):
