06fde93e9efdbe49b73385449b219877f9838e50
taoting <335354289@qq.com>
2020-12-18 21:19:42 +0800

    user_id无法为空

--- app/models/project.py
+++ app/models/project.py
@@ -8,7 +8,7 @@ class Project(Base):
     __tablename__ = 'project'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey(User.id))
+    user_id = Column(Integer, ForeignKey(User.id), nullable=False)
     name = Column(String(100), nullable=False)
     description = Column(String(1000))
     tag = Column(String(100))
