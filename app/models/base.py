e98064a9523c8f8b8177070f9f94cfb4bbbde508
taoting <335354289@qq.com>
2020-12-19 11:06:05 +0800

    refine

--- app/models/base.py
+++ app/models/base.py
@@ -1,4 +1,3 @@
-import datetime
 from contextlib import contextmanager
 
 from flask_sqlalchemy import BaseQuery
@@ -8,12 +7,8 @@ from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
 class SQLAlchemy(_SQLAlchemy):
     @contextmanager
     def auto_commit(self):
-        try:
-            yield
-            self.session.commit()
-        except Exception as e:
-            db.session.rollback()
-            raise e
+        yield
+        self.session.commit()
 
 
 db = SQLAlchemy(query_class=BaseQuery)
