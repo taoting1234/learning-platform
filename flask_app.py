6eaef6ba6f5fdc8a47f88c8a9efbb8d0b1992ef6
taoting <335354289@qq.com>
2020-12-20 23:52:32 +0800

    更换服务器端口

--- flask_app.py
+++ flask_app.py
@@ -73,6 +73,6 @@ def register_resource(app):
 if __name__ == '__main__':
     flask_app = create_app()
     if platform.system() == 'Linux':
-        flask_app.run(host="0.0.0.0", port=5000, debug=False)
+        flask_app.run(host="0.0.0.0", port=5555, debug=False)
     else:
         flask_app.run(host="0.0.0.0", port=5000, debug=True)
