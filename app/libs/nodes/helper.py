83919c093df178d49f83da99c58e57ad67f0b17e
taoting <335354289@qq.com>
2020-12-22 23:17:11 +0800

    删除不必要的代码

--- app/libs/nodes/helper.py
+++ app/libs/nodes/helper.py
@@ -5,11 +5,7 @@ def change_node(node):
     if node.node_type == 'input_node':
         return InputNode(
             node.id, node.node_type, node.project_id, node.in_edges,
-            node.out_edges, get_value(node.extra, 'input_file')
+            node.out_edges, node.extra.get('input_file')
         )
     # always not run
     assert False
-
-
-def get_value(extra, key):
-    return extra.get(key) if extra else None
