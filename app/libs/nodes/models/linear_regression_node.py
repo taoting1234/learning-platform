7433fc42fb94654920d5913dce9d68eb1325607b
taoting <335354289@qq.com>
2020-12-28 16:44:59 +0800

    修改评价指标

--- app/libs/nodes/models/linear_regression_node.py
+++ app/libs/nodes/models/linear_regression_node.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, median_absolute_error
 
 from app.libs.nodes.base_node import BaseNode
 
@@ -33,6 +33,7 @@ class LinearRegressionNode(BaseNode):
         model = LinearRegression()
         model.fit(x_train, y_train)
         y_pred = model.predict(x_test)
-        self.logger.info('mean_squared_error: {}'.format(mean_squared_error(y_test, y_pred)))
         self.logger.info('mean_absolute_error: {}'.format(mean_absolute_error(y_test, y_pred)))
+        self.logger.info('mean_squared_error: {}'.format(mean_squared_error(y_test, y_pred)))
+        self.logger.info('median_absolute_error: {}'.format(median_absolute_error(y_test, y_pred)))
         self.logger.info('r2_score: {}'.format(r2_score(y_test, y_pred)))
