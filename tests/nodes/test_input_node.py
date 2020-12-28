c929863602b3f865ec0f979fca63fbdc03509bcc
taoting <335354289@qq.com>
2020-12-28 23:22:29 +0800

    修改文件名

--- tests/nodes/test_input_node.py
+++ tests/nodes/test_input_node.py
@@ -33,7 +33,7 @@ def test_input_node(client):
     node_id = res.json['id']
     with open(
         pkg_resources.resource_filename(
-            'tests.files.linear_regression', 'x_data.csv'
+            'tests.files.linear_regression', 'x.csv'
         ), 'rb'
     ) as f:
         res = client.post('/file', data={'file': f, 'project_id': project_id})
@@ -41,7 +41,7 @@ def test_input_node(client):
         file1_id = res.json['id']
     with open(
         pkg_resources.resource_filename(
-            'tests.files.linear_regression', 'y_data.csv'
+            'tests.files.linear_regression', 'y.csv'
         ), 'rb'
     ) as f:
         res = client.post('/file', data={'file': f, 'project_id': project_id})
