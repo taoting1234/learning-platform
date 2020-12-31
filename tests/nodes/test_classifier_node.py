74bd2282a5adb71a6d57f71aa9c67b33f60f5000
taoting <335354289@qq.com>
2020-12-31 15:56:31 +0800

    修改测试

--- tests/nodes/test_classifier_node.py
+++ tests/nodes/test_classifier_node.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 import pkg_resources
@@ -14,7 +15,7 @@ files = [
 ]
 
 
-def test_classifier_node(client):
+def init(client, file):
     # 登录
     assert (
         client.post(
@@ -23,71 +24,74 @@ def test_classifier_node(client):
         == 201
     )
     project = Project.create(name=str(random.random()), tag="", user_id=1)
-    for file in files:
-        # 上传文件
-        with open(pkg_resources.resource_filename("tests.files", file[0]), "rb") as f:
-            res = client.post("/file", data={"file": f, "project_id": project.id})
-            assert res.status_code == 201
-            file1_id = res.json["id"]
-        with open(pkg_resources.resource_filename("tests.files", file[1]), "rb") as f:
-            res = client.post("/file", data={"file": f, "project_id": project.id})
-            assert res.status_code == 201
-            file2_id = res.json["id"]
-        # 创建节点
-        node1 = Node.create(
-            project_id=project.id,
-            node_type="input_node",
-            extra={"x_input_file": file1_id, "y_input_file": file2_id},
-        )
-        node2 = Node.create(
-            project_id=project.id,
-            node_type="data_split_node",
-            extra={"test_ratio": 0.2, "random_state": 888},
-        )
-        node3 = Node.create(project_id=project.id, node_type="classifier_node")
-        # 创建边
-        assert (
-            client.post(
-                "/node/edge",
-                data={
-                    "project_id": project.id,
-                    "node1_id": node1.id,
-                    "node2_id": node2.id,
-                },
-            ).status_code
-            == 201
-        )
-        assert (
-            client.post(
-                "/node/edge",
-                data={
-                    "project_id": project.id,
-                    "node1_id": node2.id,
-                    "node2_id": node3.id,
-                },
-            ).status_code
-            == 201
-        )
-        # 测试模型
-        logistic_regression(node3)
-        k_neighbors_classifier(node3)
-
-
-def logistic_regression(node):
-    node.modify(extra={"model": "LogisticRegression", "model_kwargs": {}})
-    node.run()
-
-
-def k_neighbors_classifier(node):
-    node.modify(
-        extra={"model": "KNeighborsClassifier", "model_kwargs": {"n_neighbors": 3}}
+    # 上传文件
+    with open(pkg_resources.resource_filename("tests.files", file[0]), "rb") as f:
+        res = client.post("/file", data={"file": f, "project_id": project.id})
+        assert res.status_code == 201
+        file1_id = res.json["id"]
+    with open(pkg_resources.resource_filename("tests.files", file[1]), "rb") as f:
+        res = client.post("/file", data={"file": f, "project_id": project.id})
+        assert res.status_code == 201
+        file2_id = res.json["id"]
+    # 创建节点
+    node1 = Node.create(
+        project_id=project.id,
+        node_type="input_node",
+        extra={"x_input_file": file1_id, "y_input_file": file2_id},
     )
-    node.run()
-    node.modify(
-        extra={"model": "KNeighborsClassifier", "model_kwargs": {"n_neighbors": 4}}
+    node2 = Node.create(
+        project_id=project.id,
+        node_type="data_split_node",
+        extra={"test_ratio": 0.2, "random_state": 888},
     )
-    node.run()
-    node.modify(
-        extra={"model": "KNeighborsClassifier", "model_kwargs": {"n_neighbors": 5}}
+    node3 = Node.create(project_id=project.id, node_type="classifier_node")
+    # 创建边
+    assert (
+        client.post(
+            "/node/edge",
+            data={
+                "project_id": project.id,
+                "node1_id": node1.id,
+                "node2_id": node2.id,
+            },
+        ).status_code
+        == 201
     )
-    node.run()
+    assert (
+        client.post(
+            "/node/edge",
+            data={
+                "project_id": project.id,
+                "node1_id": node2.id,
+                "node2_id": node3.id,
+            },
+        ).status_code
+        == 201
+    )
+    return node3
+
+
+def test_logistic_regression(client):
+    model_kwargs_list = [{"max_iter": 100}, {"max_iter": 1000}]
+    for file in files[: None if os.environ.get("COMPLETE_TEST") else 1]:
+        for model_kwargs in model_kwargs_list[
+            : None if os.environ.get("COMPLETE_TEST") else 1
+        ]:
+            node = init(client, file)
+            node.modify(
+                extra={"model": "LogisticRegression", "model_kwargs": model_kwargs}
+            )
+            node.run()
+
+
+def k_neighbors_classifier(client):
+    model_kwargs_list = [{"n_neighbors": 3}, {"n_neighbors": 4}, {"n_neighbors": 5}]
+    for file in files[: None if os.environ.get("COMPLETE_TEST") else 1]:
+        for model_kwargs in model_kwargs_list[
+            : None if os.environ.get("COMPLETE_TEST") else 1
+        ]:
+            node = init(client, file)
+            node.modify(
+                extra={"model": "KNeighborsClassifier", "model_kwargs": model_kwargs}
+            )
+            node.run()
