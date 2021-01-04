e7e9f2aca1282910727e36727ca6409d9a3f2176
taoting <335354289@qq.com>
2021-01-04 20:49:00 +0800

    refine

--- tests/libs/test_helper.py
+++ tests/libs/test_helper.py
@@ -1,6 +1,26 @@
 import pytest
 
 from app.libs.helper import change_columns
+from app.libs.parser import Parser
+
+
+def test_parser():
+    parser = Parser("123", type_=int)
+    with pytest.raises(Exception):
+        parser.check("xxxx")
+    parser = Parser("123", type_=int, required=True)
+    with pytest.raises(Exception):
+        parser.check(None)
+    parser = Parser("123", type_=int, range_=(0, 10))
+    with pytest.raises(Exception):
+        parser.check(-1)
+    with pytest.raises(Exception):
+        parser.check(11)
+    parser = Parser("123", type_=int, enum=[0, 1])
+    with pytest.raises(Exception):
+        parser.check(-1)
+    with pytest.raises(Exception):
+        parser.check(2)
 
 
 def test_change_columns():
