07450399d10b365b7b5c9e80fce0ae80c2730913
taoting <335354289@qq.com>
2021-01-05 12:55:04 +0800

    refine

--- app/nodes/input_node.py
+++ app/nodes/input_node.py
@@ -26,19 +26,19 @@ class NotSplitInputNode(InputNode):
     params = [
         *InputNode.params,
         Parser("x_input_file", type_=int, required=True),
-        Parser("output_columns", type_=str, required=True),
+        Parser("label_columns", type_=str, required=True),
     ]
 
     def __init__(self, id_, node_type, project_id, in_edges, out_edges, extra):
         super().__init__(id_, node_type, project_id, in_edges, out_edges, extra)
         self.x_input_file = File.get_by_id(self.x_input_file)
         assert self.x_input_file, "x_input_file not found"
-        self.output_columns = change_columns(self.output_columns)
+        self.label_columns = change_columns(self.label_columns)
 
     def run(self):
         x_df = pd.read_csv(self.x_input_file.path, header=self.header)
-        y_df = x_df[self.output_columns]
-        x_df.drop(self.output_columns, axis=1, inplace=True)
+        y_df = x_df[self.label_columns]
+        x_df.drop(self.label_columns, axis=1, inplace=True)
         self.output_shape = [x_df.shape, y_df.shape]
         # TODO UT需要，写完填充节点后删除
         x_df = x_df.fillna(value=0)
