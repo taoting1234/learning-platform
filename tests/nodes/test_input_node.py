7ca706c43f756a6c0549e52c6f4d423dbdff967e
taoting <335354289@qq.com>
2021-01-06 11:33:39 +0800

    新增ut

--- tests/nodes/test_input_node.py
+++ tests/nodes/test_input_node.py
@@ -3,12 +3,13 @@ import os
 import random
 
 import pkg_resources
+import pytest
 from flask import current_app
 
 from ..base import client
 
 
-def test_input_node(client):
+def test_split_input_node(client):
     assert (
         client.post(
             "/session", data={"username": "user1", "password": "123"}
@@ -40,6 +41,60 @@ def test_input_node(client):
                         "has_header": False,
                         "x_input_file": file1_id,
                         "y_input_file": file2_id,
+                    }
+                )
+            },
+        ).status_code
+        == 200
+    )
+    assert client.post("/project/{}/run".format(project_id)).status_code == 201
+    # 确认是否成功
+    assert client.get("/node/{}".format(node_id)).json["input_shape"] == []
+    assert client.get("/node/{}".format(node_id)).json["output_shape"] == [
+        [50, 500],
+        [50, 1],
+    ]
+    assert os.path.exists(
+        "{}/{}/node/{}/x.csv".format(
+            current_app.config["FILE_DIRECTORY"], project_id, node_id
+        )
+    )
+    assert os.path.exists(
+        "{}/{}/node/{}/y.csv".format(
+            current_app.config["FILE_DIRECTORY"], project_id, node_id
+        )
+    )
+
+
+@pytest.mark.skip()
+def test_not_split_input_node(client):
+    assert (
+        client.post(
+            "/session", data={"username": "user1", "password": "123"}
+        ).status_code
+        == 201
+    )
+    res = client.post("/project", data={"name": str(random.random()), "tag": "test"})
+    assert res.status_code == 201
+    project_id = res.json["id"]
+    res = client.post(
+        "/node", data={"project_id": project_id, "node_type": "not_split_input_node"}
+    )
+    assert res.status_code == 201
+    node_id = res.json["id"]
+    with open(pkg_resources.resource_filename("tests.files", "telco.csv"), "rb") as f:
+        res = client.post("/file", data={"file": f, "project_id": project_id})
+        assert res.status_code == 201
+        file_id = res.json["id"]
+    assert (
+        client.put(
+            "/node/{}".format(node_id),
+            data={
+                "extra": json.dumps(
+                    {
+                        "has_header": True,
+                        "x_input_file": file_id,
+                        "label_columns": "[-1]",
                     }
                 )
             },
