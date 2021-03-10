674726c7ec14668798614b773ff7bde11ba5df1f
taoting <335354289@qq.com>
2021-03-10 23:13:00 +0800

    新增返回字段

--- app/fields/user.py
+++ app/fields/user.py
@@ -5,7 +5,9 @@ from app.fields.search import meta_field
 user_field = {
     "id": fields.Integer,
     "username": fields.String,
+    "organization": fields.String,
     "permission": fields.Integer,
+    "block": fields.Integer,
 }
 
 user_search_field = {
