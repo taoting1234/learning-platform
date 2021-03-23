040bb795c02d437cbad4dd3faf8a57c340312538
taoting <335354289@qq.com>
2021-03-23 09:44:44 +0800

    fix bugs

--- task/nodes/regressor_node.py
+++ task/nodes/regressor_node.py
@@ -1,4 +1,38 @@
-from .metric import get_metric
+from sklearn.metrics import (
+    accuracy_score,
+    average_precision_score,
+    f1_score,
+    log_loss,
+    mean_absolute_error,
+    mean_squared_error,
+    median_absolute_error,
+    precision_score,
+    r2_score,
+    recall_score,
+    roc_auc_score,
+)
+
+
+def get_metric(type_, y_test, y_pred):
+    metric_list = [
+        [mean_absolute_error, mean_squared_error, median_absolute_error, r2_score],
+        [
+            accuracy_score,
+            average_precision_score,
+            f1_score,
+            log_loss,
+            precision_score,
+            recall_score,
+            roc_auc_score,
+        ],
+        [accuracy_score],
+    ]
+    # 1 回归 2 二分类 3 多分类
+    for metric in metric_list[type_ - 1]:
+        try:
+            print("{}: {}".format(metric.__name__, metric(y_test, y_pred)))
+        except ValueError:  # pragma: no cover
+            print("{}: calculate error".format(metric.__name__))
 
 
 def run(input_files, kwargs):
